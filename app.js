b4d2f0331b2f7289c71e56c03f5c3713579ee70d
seandevs <seanpdevlin@gmail.com>
2013-10-23 00:44:22 -0400

    removes extra stuff

--- app.js
+++ app.js
@@ -27,82 +27,6 @@ app.get('/:ip/:password/:action/:actionNumber', function(req, response) {
 
 });
 
-
-app.get('/videoDog', function(req, res) {
-
-    // var boundary = "BoundaryString";
-
-    var options = {
-    // host to forward to
-    host:   '10.5.5.9',
-    // port to forward to
-    port:   8080,
-    // path to forward to
-    path:   '/live/amba.m3u8',
-    // request method
-    method: 'GET',
-    // headers to send
-    headers: req.headers
-  };
-
-  var creq = http.request(options, function(cres) {
-
-    res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
-    // res.setHeader('Content-Type', 'multipart/x-mixed-replace');
-    res.setHeader('Connection', 'close');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Cache-Control', 'no-cache, private');
-    res.setHeader('Expires', 0);
-    res.setHeader('Max-Age', 0);
-
-    // wait for data
-    var body = "";
-    cres.on('data', function(chunk){
-      console.log("cres.on data");
-      body += chunk;
-    });
-
-    cres.on('end', function(){
-      var stream = new Stream();
-      var parser = m3u8.createStream();
-      stream.pipe = function(dest) {
-        console.log('stream.pipe');
-        console.log(body);
-        dest.write(body);
-        console.log(dest);
-      };
-
-      console.log('cres.on end');
-
-      stream.pipe(parser);
-
-      parser.on('item', function(item) {
-        console.log('parse on item');
-        item.set('uri', 'http://10.5.5.9/live/' + item.get('uri'));
-      });
-
-      parser.on('m3u', function(m3u) {
-
-        console.log('parser on m3u');
-        res.write(m3u);
-        res.writeHead(cres.statusCode);
-        res.end();
-      });
-
-    });
-
-  }).on('error', function(e) {
-    // we got an error, return 500 error to client and log error
-    console.log(e.message);
-    res.writeHead(500);
-    res.end();
-  });
-
-  creq.end();
-
-});
-
-
 var buffer = hls('http://10.5.5.9:8080/live/amba.m3u8');
 
 app.get('/index.m3u8', function(request, response) {
@@ -120,23 +44,6 @@ app.get('/:hash.ts', function(request, response) {
   response.setHeader('Content-Type', 'video/mp2s');
   stream.pipe(response);
 });
-
-// app.get('/video', function(req, res) {
-//     request({
-//       method: 'GET',
-//       uri: 'http://10.5.5.9:8080/live/amba.m3u8'
-//     }, function (error, res, body) {
-//       response.header('Access-Control-Allow-Origin', "*");
-//       console.log(response);
-//       response.writeHead('temp');
-//       response.end();
-//     });
-
-//   // var remoteUrl = remote + req.url;
-//   // request(remoteUrl).pipe(res);
-
-
-// });
 
 app.listen(8080);
 
