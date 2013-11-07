ad1e7ba3648bcf4aee494cbb4c4fcd59b7ec908a
Dan Mundy <drm.mundy@gmail.com>
2013-11-07 16:26:34 +0000

    Adds a comment

--- app.js
+++ app.js
@@ -239,6 +239,7 @@ app.get('/status', function(req, res) {
     })(command);
   }
 
+  // After all 3 requests complete (successfully or not), return what we have.
   var finished = _.after(3, function() {
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.write(JSON.stringify(status));
